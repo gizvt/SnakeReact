d68696c320d68b658c2570a80a4557b3f7f08c9a
Will Naylor <pop_fliz@msn.com>
2022-02-05 15:39:55 +0000

    Add margin to logo

--- src/components/Status/Title.tsx
+++ src/components/Status/Title.tsx
@@ -4,7 +4,7 @@ import reactLogo from "../../assets/logo192.png";
 export function Title() {
     return (
         <span className="h2 display-6">
-            <Image src={reactLogo} alt="" height={75} />
+            <Image src={reactLogo} alt="" height={75} className="me-2" />
             SnakeReact
         </span>
     );
