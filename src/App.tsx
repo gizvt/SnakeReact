26450c2a7c06204771c2fa507aa50878d0209cfb
Will Naylor <pop_fliz@msn.com>
2022-06-11 23:15:01 +0100

    Init audio event listeners in useEffect to stop sounds playing twice.
    - I think this could have been because React.StrictMode would render the App component twice (as it does) causing each event listener to be added twice.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Route, Routes } from "react-router-dom";
 import { AudioPlayer } from "./modules";
 import { Game } from "./pages/Game/Game";
@@ -8,9 +8,15 @@ import { MainMenu } from "./pages/MainMenu/MainMenu";
 export const App = React.memo(function () {
     // console.log("Rendering App");
     const audioPlayer = useRef(new AudioPlayer());
-    audioPlayer.current.init();
+
+    useEffect(() => {
+        audioPlayer.current.init();
+    }, []);
 
     return (
+        // Apply strict mode here instead of in index.txs to stop the audio
+        // from playing twice due to strict mode's double rendering of the App
+        // component (where the audio player exists inside a useRef).
         <Routes>
             <Route path="/" element={<MainMenu />}></Route>
             <Route path="/game" element={<Game />}></Route>
