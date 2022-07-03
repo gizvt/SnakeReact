e6ca3f90a2ac6d768ff71fd8894316d8aeec8637
Will Naylor <pop_fliz@msn.com>
2022-07-03 18:03:44 +0100

    Blur start button when it becomes disabled
    - This ensures the keydown events aren't ignored in Firefox when starting the game if you don't manually unfocus the button (e.g. by clicking somewhere else)

--- src/components/Game/BottomBar.tsx
+++ src/components/Game/BottomBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Col, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
@@ -9,6 +9,16 @@ interface Props {
 
 export const GameBottomBar = React.memo(function (props: Props) {
     const navigate = useNavigate();
+    const startButton = useRef<HTMLButtonElement>(null);
+
+    // This will remove focus from the button if we are disabling it. This is
+    // to prevent the keydown events from being fired in Firefox when the
+    // button is clicked and it becomes disabled. Firefox keeps focus on the
+    // button and will not register the keydown events until clicking somewhere
+    // else (thus un-focusing the button, as we are doing here).
+    if (props.disableStartButton) {
+        startButton.current?.blur();
+    }
 
     return (
         <Row className="mt-4 text-center">
@@ -24,6 +34,7 @@ export const GameBottomBar = React.memo(function (props: Props) {
                 <Button
                     onClick={props.handleStartGame}
                     disabled={props.disableStartButton}
+                    ref={startButton}
                 >
                     Start Game
                 </Button>
